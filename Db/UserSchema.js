134c37a9f8a49b8e9874efeddd3c95a8a9388de6
dsitdykov <deividas.sitdykov@gmail.com>
2023-11-20 22:15:22 +0200

    login-backend

--- Db/UserSchema.js
+++ Db/UserSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Library')
+mongoose.connect('mongodb://localhost:27017/Library', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
--- server.js
+++ server.js
@@ -1,20 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
-// const BookController = require("./Controllers/BookController");
-// // const ControllerName = require('path/to/Controller');
-// require('dotenv').config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(cookieParser());
-// app.use('/book', BookController);
-// // app.use('/route', ControllerName);
-
-// const PORT = process.env.PORT || 3004;
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
@@ -26,11 +9,11 @@ const app = express();
 
 app.use(cors());
 app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/book', BookController);
-app.use('/user', UserController); // Use the authentication controller
+app.use('/user', UserController);
 
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
